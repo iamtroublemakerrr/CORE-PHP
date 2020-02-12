bf6e17132c5b437f000dd8263d24164076c88ca6
STUART <55537549+iamtroublemakerrr@users.noreply.github.com>
2020-02-12 17:53:12 +0530

    Detail_queries.php

--- Detail_queries.php
+++ Detail_queries.php
@@ -4,7 +4,7 @@
   include("Connection.php");
   
   
-<!--now we run first INSERT QUERY -->
+<!--now we start and  run first INSERT QUERY -->
   if(isset($_POST["buttoname"])){
     
     <!--here we contain the all data in variabe from page to in variables-->
@@ -12,9 +12,18 @@
     $email=$_POST["email"];
     $password=$_POST["password"];
     $city=$_POST["city"];
+    $mobile=$_POST["mobile"];
+    
+      <!--INSERT QUERY-->
+  $insert="INSERT INTO tablename(name,email,password,city,mobile)VALUES('$name','$email','$password','$city','$mobile')";      //(name,email...etc)itsa table field  || ('$name','$email',etc..)its your variable which is insert in table using insert query//
 
-
-
+    <!--check the query and data inserted or not-->
+      if(mysqli_query($con,$insert)){
+        echo"DATA INSERTED";
+      }else{
+        die("DATA INSERTED FAILED".mysqli_error($con));
+      }
+<!--end of insert query-->
 
 
 
