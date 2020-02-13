13ba27b0e41994342f6c749ecb5bad6fd8cc2459
STUART <55537549+iamtroublemakerrr@users.noreply.github.com>
2020-02-13 13:11:10 +0530

    Admin_Dashboard.php
    How to fetch the data from database and make a admin pannel with delete and edit.

---
+++ Admin_Dashboard.php
@@ -0,0 +1,51 @@
+<?php
+//first we include  the Connection File here //
+  include("Connection.php");
+  
+//here we fetch the data from database using SELECT QUERY and show in admin table as a control pannel//
+  $a="SELECT * FORM tablename";
+  $data=mysqli_query($con,$a);
+?>
+
+
+<html>
+<head>
+  <tittle>ADMIN DASHBOARD</tittle>
+</head>
+<body>
+  <div class="heading">
+    <h1>WELCOME TO ADMIN DASHBOARD</h1>
+  </div>
+  
+  //here we start the print and display the data and make control pannel on ADMIN DASHBOARD//
+  <form>
+    <table>
+      <t>
+        <th>sr.no</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Mobile</th>
+        <th>Image</th>
+      </t>
+                            //here is while loope//  
+  <?php
+        while($row=mysqli_fetch_assoc($data)){
+    ?>
+          <tr>
+            <td><?php echo $row['id'];?></td>
+            <td><?php echo $row['name'];?></td>
+            <td><?php echo $row['email'];?></td>
+            <td><?php echo $row['password'];?></td>
+            <td><?php echo $row['mobile'];?></td>
+            <td><?php echo $row['image'];?></td>
+            <td><a href="Edit.php?id=<?php eho $row['id'];?>">Edit</a></td>        //here is we ref the edit page and then go to fo edit//
+            <td><a href="Delete.php?id=<?php echo $row['id'];?>">Delete</a></td>   //here is delete query page where they click the delete row has to be deleted//
+          </tr>
+       <?php
+       }
+       ?>
+  </form>
+  
+</body>
+</html>
